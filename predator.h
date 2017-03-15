9216da521ff64c01c3d3964d8a5294e2525eec5e
Ashwary <vshlkumar56@gmail.com>
2017-03-16 01:37:27 +0530

    Change access type of move from public to private

--- predator.h
+++ predator.h
@@ -9,6 +9,7 @@ and behaviour.
 #include "grid.h"				//To import grid and grid functions
 #include "objective.h"			//To get the objective functions
 #include <math.h>
+#include <stdlib.h>				//Random number generator
 
 class predator{
 	private:
@@ -16,12 +17,14 @@ class predator{
 		int allowed_moves;			//Moves allowed by predator in that iteration	
 		double *func_weights;		//Weights of a particular objective function
 		int no_objectives;			//Total number of objective function
+
+		void move(Grid &grid);
+		
 	public:
 
 		predator(Point a);
 		Point get_position();
-		void move();
-		void attack();
+		void attack(Grid &grid);
 
 };
 
